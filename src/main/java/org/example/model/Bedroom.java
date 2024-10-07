89b052634b7874e2a6d1c27f7e422c11d139d98b
cmlutkayd <utkuaydogdu@hotmail.com>
2024-10-08 00:09:34 +0300

    FSWEB S14D2 çözüm

---
+++ src/main/java/org/example/model/Bedroom.java
@@ -0,0 +1,49 @@
+package org.example.model;
+
+public class Bedroom extends Room {
+
+    private  String name;
+    private  Bed bed;
+    private  Wardrobe wardrobe;
+
+    public Bedroom(Wall wall1, Wall wall2, Wall wall3, Wall wall4,
+                   Ceiling ceiling, Lamp lamp, Carpet carpet, String name, Bed bed,Wardrobe wardrobe) {
+        super(wall1, wall2, wall3, wall4, ceiling, lamp, carpet);
+        this.name = name;
+        this.bed = bed;
+        this.wardrobe = wardrobe;
+    }
+
+    public  void createBedroom() {
+        getWall1().create();
+        getWall2().create();
+        getWall3().create();
+        getWall4().create();
+        bed.make();
+        getCarpet().lying();
+        wardrobe.add();
+        getLamp().turnOn();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBed(Bed bed) {
+        this.bed = bed;
+    }
+
+    public void setWardrobe(Wardrobe wardrobe) {
+        this.wardrobe = wardrobe;
+    }
+
+    @Override
+    public String toString() {
+        return "Bedroom{" +
+                "name='" + name + '\'' +
+                ", bed=" + bed +
+                ", wardrobe=" + wardrobe +
+                ", room= "+ super.toString() +
+                '}';
+    }
+}
